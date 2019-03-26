e226d3cdb0e6683ee23d8002e421eecc19a33793
Chao Ye <Chao12.ye@gmail.com>
2019-03-26 11:42:03 -0700

    add chips and soda routes

--- src/Chips.js
+++ src/Chips.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class Chips extends Component {
     render() {
         return (
             <div>
-                
+               
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyTk32kLWmHR3u0OcDj0k6LHk6-A9QZmD0aR3xuplkxAqQLo9Pwg" alt="CRUNCHY nom nom nom" />
+                <div>
+                    <h1>SPICY Chips...OMG</h1>
+                    <p><Link to="/"> Burp, GO BAAAAACK</Link></p>
+
+                </div>
             </div>
+        
         );
     }
 }
--- src/Soda.js
+++ src/Soda.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class Soda extends Component {
     render() {
         return (
             <div>
-                
+                <img src="http://4.bp.blogspot.com/-tGTW4_ldi14/UXq5ipMO6UI/AAAAAAAACRI/7JImLR36gug/s1600/FANTA+003.gif" alt="So delicious though"/> 
+                <div>
+                    <h1>OMG SSSSUUUUUGAR</h1>
+                    <p><Link to="/"> GO BAAAAACK</Link></p>
+
+                </div>
             </div>
         );
     }
