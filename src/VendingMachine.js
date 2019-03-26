9c55d94a8bad0a40608c3bb2fa2e9febf7b325fa
Chao Ye <Chao12.ye@gmail.com>
2019-03-26 11:04:00 -0700

    add VendingMachine Module

---
+++ src/VendingMachine.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from '/Home';
+import Soda from '/Soda';
+import Chips from '/Chips';
+import Sardines from '/Sardines';
+import NavBar from './NavBar';
+
+
+class VendingMachine extends Component {
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <NavBar />
+                    <Switch>
+                        <Route exact path='/' render={ () => <Home /> } />
+                        <Route exact path='/soda' render={ () => <Soda /> } />
+                        <Route exact path='/chips' render={ () => <Chips /> } />
+                        <Route exact path='/sardines' render={ () => <Sardines /> } />
+                    </Switch >
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default VendingMachine;
